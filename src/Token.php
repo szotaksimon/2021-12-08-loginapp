573e569f259abfbdfde98245d5785ebf8a6bfbfb
Szoták Simon <szotak.simon16@gmail.com>
2021-12-08 13:15:44 +0100

    token.php init

---
+++ src/Token.php
@@ -0,0 +1,9 @@
+<?php
+namespace Petrik\Loginapp;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Token extends Model {
+    // ha a created_at / updated_at nélkül hoztuk létre:  ->
+        //protected $timestamps = false;
+}
